f81cca565f96b07d04cdc06e3bd65ce23ab49a4c
otalorajuand <otalorajuand@gmail.com>
2023-05-15 20:55:47 -0500

    Adjustment of response text

--- image_recognition_api/front/script.js
+++ image_recognition_api/front/script.js
@@ -1,54 +1,47 @@
 // Get the input element for image upload
-const inputElement = document.getElementById("image_upload");
+const imageUpload = document.getElementById("image_upload");
 const responseContainer = document.getElementById("response_container");
+const imageContainer = document.getElementById('image_container');
 
-// Add an event listener to the input element to listen for when an image is uploaded
-inputElement.addEventListener("change", (event) => {
-  // Get the uploaded file
-  const file = event.target.files[0];
-
-  // Create a new FileReader object
-  const reader = new FileReader();
-
-  // Add an event listener to the FileReader to listen for when the file has finished loading
-  reader.addEventListener("load", (event) => {
-    // Get the loaded image data as a URL
-    const imageDataUrl = event.target.result;
-
-    // Create a new image element and set its source to the loaded image data URL
-    const imageElement = document.createElement("img");
-    imageElement.src = imageDataUrl;
-
-    // Get the previous image element, if any, and remove it
-    const previousImageElement = inputElement.parentNode.querySelector("img");
-    if (previousImageElement) {
-      previousImageElement.remove();
+// Add event listener to the file input
+imageUpload.addEventListener('change', function(e) {
+    // Remove the previously displayed image, if any
+    while (imageContainer.firstChild) {
+        imageContainer.firstChild.remove();
     }
 
-    // Insert the image element before the input element
-    inputElement.parentNode.insertBefore(imageElement, inputElement);
-  });
+    // Create a new image element
+    const img = document.createElement('img');
+    img.style.maxWidth = '100%';
+    img.style.maxHeight = '100%';
 
-  // Read the uploaded file as a data URL
-  reader.readAsDataURL(file);
+    // Set the source of the image to the uploaded file
+    img.src = URL.createObjectURL(e.target.files[0]);
 
-  // Create a new FormData object
-  const formData = new FormData();
+    // Append the image to the image container
+    imageContainer.appendChild(img);
 
-  // Add the file to the FormData object with the name "file"
-  formData.append("file", file);
+    // Read the uploaded file as a data URL
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
 
-  // Send the POST request to the API with the FormData as the body
-  fetch("http://localhost:8000/api/predict", {
-    method: "POST",
-    body: formData,
-  })
-  .then(response => response.text())
-  .then(responseText => {
-      responseContainer.innerHTML = responseText;
-  })
-  .catch(error => {
-      console.error(error);
-      responseContainer.innerText = "An error occurred: " + error.message;
-  });
+    // Create a new FormData object
+    const formData = new FormData();
+
+    // Add the file to the FormData object with the name "file"
+    formData.append("file", e.target.files[0]);
+
+    // Send the POST request to the API with the FormData as the body
+    fetch("http://localhost:8000/api/predict", {
+        method: "POST",
+        body: formData,
+    })
+    .then(response => response.text())
+    .then(responseText => {
+        responseContainer.innerHTML = responseText.replace(/['"]+/g, '');
+    })
+    .catch(error => {
+        console.error(error);
+        responseContainer.innerText = "An error occurred: " + error.message;
+    });
 });
